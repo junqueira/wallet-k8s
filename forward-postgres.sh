86f28e18ea87db0f7178348628d7cc800e85cc9c
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2020-04-25 05:12:56 -0300

    add commit

--- forward-postgres.sh
+++
@@ -1,9 +0,0 @@
-
-# opa, pod postgres tem acesso brindado, e ele ficar disponivel com acesso 
-# externo ao cluster tem que fazer um port forward 
-
-kubectl port-forward \
-$(kubectl get pods --all-namespaces|grep postgres|awk '{print $2}') \
-5432:5432
-
-# kubectl proxy --port=8080
